00ecd11e3b83af5bf97df1d368dafd664246a980
Jered Wierzbicki <jeredw@gmail.com>
2013-06-22 13:57:46 -0700

    checkpoint

--- cavern.s
+++ cavern.s
@@ -2,125 +2,70 @@
         include "vcs.h"
         include "macro.h"
 
-COUNT   EQU $80
-SCOLOR  EQU $81
+PATTERN EQU $80
+TIMECH  EQU 20
 
         SEG
         ORG $F000
 
 Reset
-        sta SCOLOR
-StartOfFrame
+        CLEAN_START
 
-        ; Vertical blanking
-        lda #0
-        sta VBLANK
+        sta PATTERN
+        lda #$45
+        sta COLUPF
+
+StartOfFrame
+        ; Enable vertical sync generation.
         lda #2
         sta VSYNC
 
-        ; 3 scanlines of vsync
+        ; Draw 3 scanlines of vsync to trigger retrace.
+        ; VBLANK bit 1 should be set to black the screen since some TVs don't
+        ; understand vsync unless blacking.
         sta WSYNC
         sta WSYNC
         sta WSYNC
 
+        ; Turn off vertical sync gen.
         lda #0
         sta VSYNC
 
-        ; 37 scanlines of vertical blank
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
+        ; Draw 37 scanlines of vertical blank.
+        ldx #37
+VBlank  sta WSYNC
+        dex
+        bne VBlank
+        stx VBLANK     ; Re-enable video output.
+
+        iny
+        cpy TIMECH
+        bne notyet
+        ldy #0
+        inc PATTERN
+notyet
 
         ; 192 scanlines of picture
-        ldx #0
-        ldy #0
-        REPEAT 192; scanlines
-          inx
-          stx COLUBK
-          nop
-          nop
-          nop
-          nop
-          nop
-          nop
-          nop
-          nop
-          nop
-          nop
-          dey
-          sty COLUBK
-          sta WSYNC
-        REPEND
-        inc SCOLOR
+        ldx #192
+Picture stx COLUPF
+        sta WSYNC
+        lda PATTERN
+        sta PF1
+        SLEEP 40
+        lda #0
+        sta PF1
+        dex
+        bne Picture
 
         lda #%01000010
         sta VBLANK        ; end of scren
 
         ; 30 scanlines of overscan
+        ldx #30
+Overscan
         sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
-        sta WSYNC
+        dex
+        bne Overscan
         
         jmp StartOfFrame
 
